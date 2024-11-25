afb98f6846e53afb8121462620f88097cced1bdb
zikajoao <joao.goncalves.silva05@escola.pr.gov.br>
2024-11-25 14:34:23 +0000

    25/11

--- perguntas.js
+++ perguntas.js
@@ -1,33 +1,33 @@
 criaCartao(
-    'Programação',
+    '1',
     'O que é Python?',
     'O Python é uma linguagem de programação'
 )
 
 criaCartao(
-    'Programação',
+    '2',
     'O que é JavaScript?',
     'O JavaScript é uma linguagem de programação.'
 )
 
 criaCartao(
-    'Programação',
+    '3',
     'O que é CSS?',
     'O CSS é uma lingugem de estilização!'
 )
 
 criaCartao(
-    'Programação',
+    '4',
     'O que é HTML?',
     'O HTML é uma lingugem de estruturação e semântica do conteúdo.'
 )
 criaCartao(
-    'Programação',
+    '5',
     'O que é VSCode?',
     'O Visual Studio Code é um editor de código com diversas integrações.s'
 )
 criaCartao(
-    'Programação',
+    '6',
     'O que é GitHub?',
     'O GitHub é uma plataforma de desenvolvimento colaborativo que aloja projetos na nuvem.'
 )
