8532c87a3b9c77d9c8d9ae642e29fe0ce8a2036e
zikajoao <joao.goncalves.silva05@escola.pr.gov.br>
2024-11-25 14:41:42 +0000

    25/11

--- perguntas.js
+++ perguntas.js
@@ -1,33 +1,33 @@
 criaCartao(
     '1',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'maio time do brasil?',
+    'flamengo'
 )
 
 criaCartao(
     '2',
-    'O que é JavaScript?',
-    'O JavaScript é uma linguagem de programação.'
+    'melhor tecnico do flamengo?',
+    'jorge jesus'
 )
 
 criaCartao(
     '3',
-    'O que é CSS?',
-    'O CSS é uma lingugem de estilização!'
+    'melhor atacante do flamengo?',
+    'pedro'
 )
 
 criaCartao(
     '4',
-    'O que é HTML?',
-    'O HTML é uma lingugem de estruturação e semântica do conteúdo.'
+    'melhor goleiro ?',
+    'rossi'
 )
 criaCartao(
     '5',
-    'O que é VSCode?',
-    'O Visual Studio Code é um editor de código com diversas integrações.s'
+    'quantos mundial?',
+    '1 mundial 1981'
 )
 criaCartao(
     '6',
-    'O que é GitHub?',
-    'O GitHub é uma plataforma de desenvolvimento colaborativo que aloja projetos na nuvem.'
+    'melhor zagueiro?',
+    'léo perreira'
 )
